b9e73c65776287c9ca12eb6ddba34357bb5783fc
Stratos Idreos <stratos@Stratoss-MacBook-Pro.local>
2019-11-28 00:12:53 -0500

    initial commit for 2020

---
+++ 2020/js/route.js
@@ -0,0 +1,44 @@
+ApplicationRouter = Backbone.Marionette.AppRouter.extend({
+	routes : {
+		"" : "home",
+		"home" : "home",
+		"program" : "program",
+		"papers" : "papers",
+		"keynote" : "keynote",
+		"posters" : "posters"
+	},
+
+	deselectPills : function() {
+		$('ul.sidebar li').removeClass('active');
+	},
+
+	selectPill : function(pill) {
+		this.deselectPills();
+		$(pill).addClass('active');
+	},
+
+	home : function() {
+		this.selectPill('ul.sidebar li.home');
+		MyApp.contentRegion.show(new ContentHomeView());
+	},
+
+	keynote : function() {
+		this.selectPill('ul.sidebar li.keynote');
+		MyApp.contentRegion.show(new ContentKeynoteView());
+	},
+
+	program : function() {
+		this.selectPill('ul.sidebar li.program');
+		MyApp.contentRegion.show(new ContentProgramView());
+	},
+
+	papers : function() {
+		this.selectPill('ul.sidebar li.papers');
+		MyApp.contentRegion.show(new ContentPapersView());
+	},
+
+	posters : function() {
+		this.selectPill('ul.sidebar li.posters');
+		MyApp.contentRegion.show(new ContentPostersView());
+	}
+});
